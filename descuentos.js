e2e36815b1a6bb8a5e7f4c4091aff4d509b63b2e
Lemuel Colmenares <leelcoma@gmail.com>
2023-03-23 11:27:06 -0600

    actualizar

--- descuentos.js
+++ descuentos.js
@@ -13,14 +13,19 @@ function onClickButtonPriceDiscount() {
     const priceValue = inputPrice.value;
 
     const inputDiscount = document.getElementById("inputDiscount");
-    const  discountValue = inputPrice.value;
+    const  discountValue = inputDiscount.value;
 
     const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+
+
 }
 
 //console.log({
-  //  precioOriginal,
-  //  descuento,
+  //  precioOriginal,,
+  //  descuento
   //  porcentajePrecioConDescuento,
   //  precioDescuento,
 // });
